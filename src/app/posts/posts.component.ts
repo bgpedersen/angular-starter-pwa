1eb08f5a6020213ee4a1c876df81acb126a2af0b
Bjarne <bgp@proreact.dk>
2018-04-06 10:16:20 +0200

    Cleaned files

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,22 +1,17 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent implements OnInit, OnDestroy {
+export class PostsComponent implements OnInit {
 
   constructor() {
 
   }
 
   ngOnInit() {
-  }
-
-  ngOnDestroy() {
 
   }
 
