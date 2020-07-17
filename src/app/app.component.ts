9ad8887b5bf0712d2104fb31bf335e484b5722af
Gennady Polakov <telestorage.app@gmail.com>
2020-07-18 00:57:17 +0400

    #1 Upgrade to Angular 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
         if (confirm('New version available. Load new Version?')) {
           window.location.reload();
         }
-      })
+      });
     }
 
   }
